ea73afc910807e1b3f3783e8bc6a93f28fbaf0c1
Jeff Brubaker <jtbrubak1@gmail.com>
2017-05-08 05:36:52 -0400

    Fix bubbles not sticking

--- demo.js
+++ demo.js
@@ -10,7 +10,7 @@ function init() {
   addCannon();
   loadBubble();
   createjs.Ticker.addEventListener("tick", () => tick());
-  createjs.Ticker.setFPS(90);
+  createjs.Ticker.setFPS(60);
   this.document.onkeydown = keydown;
   this.document.onkeyup = keyup;
   stage.update();
@@ -27,9 +27,6 @@ function addCannon() {
 
 function keydown(event) {
     keys[event.keyCode] = true;
-    if (keys[37] && cannon.rotation >= -75) cannon.rotation -= 3;
-    if (keys[39] && cannon.rotation <= 75) cannon.rotation += 3;
-    if (keys[38]) cannon.rotation = 0;
     if (keys[32]) fireBubble();
 }
 
@@ -47,7 +44,8 @@ function Bubble(num) {
 }
 
 function randomBubble() {
-  return Bubble(Math.floor(Math.random() * (9 - 1)) + 1);
+  return Bubble(Math.floor(Math.random() * (7 - 1)) + 1);
+  // return Bubble(1)
 }
 
 function loadBubble() {
@@ -61,23 +59,42 @@ function move() {
   movingObjects.forEach((obj) => {
     obj.x += obj.speed[0];
     obj.y += obj.speed[1];
+    stage.update()
     if (wallDetect(obj)) obj.speed[0] *= -1;
     if (collisionDetect()) {
       bubbles[newBubble.id] = newBubble;
-      if (checkNeighbors(newBubble).length >= 2) console.log("WOOO")
+      let destroy = checkNeighbors(newBubble).concat([newBubble.id])
+      if (destroy.length >= 3) destroyBubbles(destroy)
       movingObjects = [];
+      createjs.Ticker.setFPS(60)
       loadBubble();
     }
     if (ceilingDetect()) {
       bubbles[newBubble.id] = newBubble;
       movingObjects = [];
+      createjs.Ticker.setFPS(60)
       loadBubble();
     }
   })
 }
 
+function destroyBubbles(destroy) {
+  destroy.forEach((bubble) => {
+    removeNeighbors(bubble)
+    stage.removeChild(bubbles[bubble])
+    delete bubbles[bubble]
+  })
+}
+
+function removeNeighbors(bubble) {
+  bubbles[bubble].neighbors.forEach(neighbor => {
+    let i = bubbles[neighbor].neighbors.indexOf(bubble)
+    bubbles[neighbor].neighbors.splice(i, 1)
+  })
+}
+
 function checkNeighbors(bubble, skip = []) {
-  sameColorNeighbors = [];
+  sameColorNeighbors = []
   bubble.neighbors.forEach((neighbor) => {
     if (bubble.color === bubbles[neighbor].color && !skip.includes(neighbor)) {
       sameColorNeighbors.push(neighbor)
@@ -116,23 +133,27 @@ function wallDetect() {
 }
 
 function calculateXSpeed() {
-  if (cannon.rotation <= -45) return -10;
-  else if (cannon.rotation >= 45) return 10;
-  else return (cannon.rotation / 45) * 10;
+  if (cannon.rotation <= -45) return -4;
+  else if (cannon.rotation >= 45) return 4;
+  else return (cannon.rotation / 45) * 4;
 }
 
 function calculateYSpeed() {
-  if (cannon.rotation >= -45 && cannon.rotation <= 45) return -10;
-  else if (cannon.rotation < -45) return ((90 + cannon.rotation) / 45) * -10;
-  else return ((90 - cannon.rotation) / 45) * -10;
+  if (cannon.rotation >= -45 && cannon.rotation <= 45) return -4;
+  else if (cannon.rotation < -45) return ((90 + cannon.rotation) / 45) * -4;
+  else return ((90 - cannon.rotation) / 45) * -4;
 }
 
 function fireBubble() {
   newBubble.speed = [calculateXSpeed(), calculateYSpeed()];
   movingObjects.push(newBubble);
+  createjs.Ticker.setFPS(200);
 }
 
 function tick() {
+  if (keys[37] && cannon.rotation >= -75) cannon.rotation -= 3;
+  if (keys[39] && cannon.rotation <= 75) cannon.rotation += 3;
+  if (keys[38]) cannon.rotation = 0;
   move();
   stage.update();
 }
