4b8d3e9a82f8aebfd0194c3c84c95285f60f623e
Jeff Brubaker <jtbrubak1@gmail.com>
2017-06-27 15:16:06 -0400

    Flesh out Board class

--- board.js
+++ board.js
@@ -6,12 +6,28 @@ class Board {
   }
 
   setupGrid() {
-    for (var i = 0; i < 13; i++) {
-      this.grid[i] = []
-      if (i % 2 === 0) { this.setupEvenRow(i) }
-      else { this.setupOddRow(i) }
+    for (var row = 0; row < 13; row++) {
+      this.grid[row] = []
+      if (row % 2 === 0) { this.setupEvenRow(row) }
+      else { this.setupOddRow(row) }
+    }
+  }
+
+  setupEvenRow(y) {
+    for (var col = 0; col < 12; col++) {
+      this.grid[row][col].x = col * 33
+      this.grid[row][col].y = y * 33
+      this.grid[row][col].bubble = null
+    }
+  }
+
+  setupOddRow(y) {
+    for (var col = 0; col < 12; col++) {
+      this.grid[row][col].x = col * 33
+      this.grid[row][col].y = (y * 33) + 16
+      this.grid[row][col].bubble = null
     }
   }
 }
 
-module.exports = Bubble;
+module.exports = Board;
